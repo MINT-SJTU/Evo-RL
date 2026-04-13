1fae673b2f9e0d378a6aabd968022c5723259508
haomin1010 <haomin1010@gmail.com>
2026-04-13 15:09:40 +0800

    v1

--- scripts/inference_socks.sh
+++ scripts/inference_socks.sh
@@ -11,15 +11,15 @@
 # 2>&1 | grep -v "ARX方舟无限"
 # --end-traj-dir /home/user/workspace/zsj/Evo-RL/traj
 # --execution-horizon 10 --RTC 
-#--joint-traj-dir traj/joint_traj/ \
 
 python -u -m lerobot.scripts.lerobot_arx5_dual_infer   \
---task "Fold the towel and put it on the edge of the table"   \
---policy-path checkpoints/towel300_0408/pretrained_model  \
+--task "Fold the socks together and place them on the edge of the desk"   \
+--policy-path checkpoints/socks200_0410/checkpoints/pretrained_model \
 --left-can-port can0 \
 --right-can-port can1 \
 --cameras base:150622073629 left_wrist:352122273179  right_wrist:409122272986 \
 --cam-width 424 --cam-height 240 \
 --duration 0.05 \
---raw-train-record-dir /home/user/workspace/zsj/datasets/arx5/raw_towel_vr \
+--raw-train-record-dir /home/user/workspace/zsj/datasets/arx5/raw_socks_0411 \
 2>&1 | grep -v "ARX方舟无限"
+## --joint-traj-dir traj/joint_traj/ 
